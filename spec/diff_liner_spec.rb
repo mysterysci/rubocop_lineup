eed9ebddb12a75fa79c564207393be25475573b7
chrismo <chrismo@clabs.org>
2018-05-02 17:21:25 -0500

    Add specs for multiple file changes

--- spec/diff_liner_spec.rb
+++ spec/diff_liner_spec.rb
@@ -4,15 +4,19 @@ RSpec.describe RubocopLineup::DiffLiner do
   context "calculates lines of uncommitted files" do
     let(:gf) { GitFixture.new }
 
-    def setup_line_edits(filename, initial_lines, new_lines)
-      gf.write_file(filename, initial_lines)
+    def setup_file_edits(hash)
+      hash.each_pair do |filename, (initial_lines, _)|
+        gf.write_file(filename, initial_lines)
+      end
       gf.commit_all
-      gf.write_file(filename, new_lines)
+      hash.each_pair do |filename, (_, new_lines)|
+        gf.write_file(filename, new_lines)
+      end
     end
 
     it "single changed line" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', 'initial content', 'updated content')
+        setup_file_edits('a.txt' => ['initial content', 'updated content'])
         # TODO: the diff call will need to be made by a production class.
         # TODO: \ swap in that class here instead of the fixture
         dl = RubocopLineup::DiffLiner.new(gf.diff)
@@ -23,7 +27,7 @@ RSpec.describe RubocopLineup::DiffLiner do
 
     it "two changed lines side-by-side" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', %w(a b c), %w(a y z))
+        setup_file_edits('a.txt' => [%w(a b c), %w(a y z)])
         dl = RubocopLineup::DiffLiner.new(gf.diff)
         expect(dl.changed_line_numbers['a.txt']).to eq [2, 3]
       end
@@ -31,7 +35,7 @@ RSpec.describe RubocopLineup::DiffLiner do
 
     it "two changed lines separated" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', %w(a b c d), %w(a * c *))
+        setup_file_edits('a.txt' => [%w(a b c d), %w(a * c *)])
         dl = RubocopLineup::DiffLiner.new(gf.diff)
         expect(dl.changed_line_numbers['a.txt']).to eq [2, 4]
       end
@@ -39,7 +43,7 @@ RSpec.describe RubocopLineup::DiffLiner do
 
     it "one line deleted" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', %w(a b c), %w(a c))
+        setup_file_edits('a.txt' => [%w(a b c), %w(a c)])
         dl = RubocopLineup::DiffLiner.new(gf.diff)
         expect(dl.changed_line_numbers['a.txt']).to eq []
       end
@@ -47,7 +51,7 @@ RSpec.describe RubocopLineup::DiffLiner do
 
     it "one line added" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', %w(a b c), %w(a b q c))
+        setup_file_edits('a.txt' => [%w(a b c), %w(a b q c)])
         dl = RubocopLineup::DiffLiner.new(gf.diff)
         expect(dl.changed_line_numbers['a.txt']).to eq [3]
       end
@@ -55,9 +59,19 @@ RSpec.describe RubocopLineup::DiffLiner do
 
     it "one add, one delete, 2 separate changes" do
       gf.make_temp_repo do |dir|
-        setup_line_edits('a.txt', %w(a b c d e f g), %w(A b d e E f G))
+        setup_file_edits('a.txt' => [%w(a aa b c d e f g), %w(A AA b d e E f G)])
         dl = RubocopLineup::DiffLiner.new(gf.diff)
-        expect(dl.changed_line_numbers['a.txt']).to eq [1, 5, 7]
+        expect(dl.changed_line_numbers['a.txt']).to eq [1, 2, 6, 8]
+      end
+    end
+
+    it "multiple files" do
+      gf.make_temp_repo do |dir|
+        setup_file_edits('a.txt' => [%w(a b c d), %w(a B c D)],
+                         'b.txt' => [%w(1 2 3 4), %w(11 22 3 4)])
+        dl = RubocopLineup::DiffLiner.new(gf.diff)
+        expect(dl.changed_line_numbers['a.txt']).to eq [2, 4]
+        expect(dl.changed_line_numbers['b.txt']).to eq [1, 2]
       end
     end
   end
