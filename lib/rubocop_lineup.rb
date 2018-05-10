e8711ea587be2e51b0d484f46b8ef3cf294a1a1b
chrismo <chrismo@clabs.org>
2018-05-10 10:50:00 -0500

    A working monkey-patch. No spec yet.

--- lib/rubocop_lineup.rb
+++ lib/rubocop_lineup.rb
@@ -10,18 +10,19 @@ module RubocopLineup
   # parent branch. There are ways to calculate the parent branch name
   # that cover common cases, but that's more complicated and may be added
   # in a future version.
-  def self.line_em_up(directory, parent_branch="master")
-    git = Git.open(directory)
-    Dir.chdir(directory) do
-      uncommitted_changes = DiffLiner.diff_uncommitted.changed_line_numbers
-      committed_changes_on_branch = DiffLiner.diff_branch(parent_branch).changed_line_numbers
+  def self.line_em_up(directory, parent_branch = "master")
+    @line_em_up ||= begin
+      Dir.chdir(directory) do
+        uncommitted_changes = DiffLiner.diff_uncommitted.changed_line_numbers
+        committed_changes_on_branch = DiffLiner.diff_branch(parent_branch).changed_line_numbers
 
-      # When a file has committed changes AND uncommitted_changes,
-      # we will only include the lines from the uncommitted changes
-      # because the lines in the previous committed changes may not
-      # be accurate anymore. See test suite for a sample case.
+        # When a file has committed changes AND uncommitted_changes,
+        # we will only include the lines from the uncommitted changes
+        # because the lines in the previous committed changes may not
+        # be accurate anymore. See test suite for a sample case.
 
-      committed_changes_on_branch.merge(uncommitted_changes)
+        committed_changes_on_branch.merge(uncommitted_changes)
+      end
     end
   end
 end
--- lib/rubocop_lineup/monkey_patch_rubocop.rb
+++ lib/rubocop_lineup/monkey_patch_rubocop.rb
@@ -1,1 +1,15 @@
-# TODO
+require 'rubocop'
+
+module DuckPunch
+  module CommentConfig
+    def cop_enabled_at_line?(cop, line_number)
+      source_file = processed_source.path.sub("#{Dir.pwd}/", '')
+      files_hash = RubocopLineup.line_em_up(Dir.pwd)
+      return false unless files_hash.key?(source_file)
+      changed_line_numbers = files_hash[source_file]
+      changed_line_numbers.include?(line_number) ? super : false
+    end
+  end
+end
+
+RuboCop::CommentConfig.prepend(DuckPunch::CommentConfig)
